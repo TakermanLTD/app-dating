588d3648405f5ec6047e1bea126913a5af9b571d
Tanyo Ivanov <tivanov@takerman.net>
2024-03-20 22:58:16 +0200

    card

--- takerman.dating.client/src/views/Dates.vue
+++ takerman.dating.client/src/views/Dates.vue
@@ -35,8 +35,14 @@
                 </div>
             </div>
             <div class="col-lg-9 col-md-8">
-                <div class="row pb-3">
-                    <!-- <div class="col-12 pb-1">
+                <div class="row pb-3 card-deck">
+                    <loader v-if="this.loading" />
+                    <div v-else-if="!dates || dates.length == 0" class="text-center">
+                        <h3>{{ $t('dates.status.nodates') }}</h3>
+                    </div>
+                    <div v-else>
+                        <card :id="date.id" v-for="(date, dateKey) in this.dates" :key="dateKey" />
+                        <!-- <div class="col-12 pb-1">
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <div>
                                 <button class="btn btn-sm btn-light"><i class="fa fa-th-large"></i></button>
@@ -62,61 +68,7 @@
                             </div>
                         </div>
                     </div> -->
-                    <loader v-if="this.loading" />
-                    <div v-else-if="!dates || dates.length == 0" class="text-center">
-                        <h3>{{ $t('dates.status.nodates') }}</h3>
                     </div>
-                    <div v-else v-for="(date, dateKey) in this.dates" :key="dateKey" class="col-lg-4 col-md-6 col-sm-6 pb-1">
-                        <router-link class="h6 text-decoration-none text-truncate" :to="'date?id=' + date.id">
-                            <div class="product-item bg-light mb-4">
-                                <div class="product-img position-relative overflow-hidden">
-                                    <img class="img-fluid w-100" src="../assets/img/date/date-thumbnail.jpeg" alt="">
-                                    <div class="product-action">
-                                        <router-link class="btn btn-outline-dark btn-square" to=""><i class="fa fa-shopping-cart"></i></router-link>
-                                        <router-link class="btn btn-outline-dark btn-square" to=""><i class="far fa-heart"></i></router-link>
-                                    </div>
-                                </div>
-                                <div class="text-center py-4">
-                                    {{ date.title }}
-                                    <div class="d-flex align-items-center justify-content-center mt-2">
-                                        <table class="tbl tbl-fluid">
-                                            <tr>
-                                                <td class="font-weight-normal">{{ $t('dates.card.ethnicity') }}:</td>
-                                                <td><strong>{{ date.ethnicity }}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td class="font-weight-normal">{{ $t('dates.card.price') }}:</td>
-                                                <td><strong>{{ date.price }} {{ this.currency }}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td class="font-weight-normal">{{ $t('dates.card.ages') }}:</td>
-                                                <td><strong>{{ date.minAges }} - {{ date.maxAges }}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td class="font-weight-normal">{{ $t('dates.card.menCount') }}:</td>
-                                                <td><strong>{{ date.menCount }}/{{ date.minMen }}</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td class="font-weight-normal">{{ $t('dates.card.womenCount') }}:</td>
-                                                <td><strong>{{ date.womenCount }}/{{ date.minWomen }}</strong></td>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </router-link>
-                    </div>
-                    <!-- <div class="col-12">
-                        <nav>
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
-                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>
-                    </div> -->
                 </div>
             </div>
         </div>
