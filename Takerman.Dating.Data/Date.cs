021710ee424bd0f65eb38656e7baabd8a5fe1645
Tanyo Ivanov <tivanov@takerman.net>
2024-04-26 00:59:37 +0300

    remove attendees

--- Takerman.Dating.Data/Date.cs
+++ Takerman.Dating.Data/Date.cs
@@ -44,7 +44,7 @@ namespace Takerman.Dating.Data
 
         public Ethnicity Ethnicity { get; set; }
 
-        public virtual ICollection<User> Attendees { get; set; } = null;
+        // public virtual ICollection<User> Attendees { get; set; } = null;
 
         public virtual ICollection<Order> Orders { get; set; } = null;
 
---
+++ Takerman.Dating.Data/Migrations/20240425215829_RemoveAttendees.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Takerman.Dating.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAttendees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Dates_DateId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_DateId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateId",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DateId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_DateId",
+                table: "Users",
+                column: "DateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Dates_DateId",
+                table: "Users",
+                column: "DateId",
+                principalTable: "Dates",
+                principalColumn: "Id");
+        }
+    }
+}
