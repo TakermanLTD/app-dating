756579b3756082715ac55f77755b8a76d0bd2765
Tanyo Ivanov <tivanov@takerman.net>
2024-06-21 17:24:06 +0300

    headers

--- Takerman.Dating.Server/Program.cs
+++ Takerman.Dating.Server/Program.cs
@@ -96,12 +96,13 @@ app.Use(async (context, next) =>
     context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
     context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
     context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
     context.Response.Headers.Add("X-Developed-By", "Takerman Ltd");
     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("X-Frame-Options", "DENY");
     context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
     context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
-    context.Response.Headers.Add("X-Powered-By", "Pegasus");
+    context.Response.Headers.Add("X-Powered-By", "Takerman");
     await next();
 });
 
