3aff68180c4b5abc702630eb7973a003bae702a0
Tanyo Ivanov <tivanov@takerman.net>
2024-06-21 17:14:25 +0300

    security

--- Takerman.Dating.Server/Program.cs
+++ Takerman.Dating.Server/Program.cs
@@ -92,17 +92,17 @@ app.UseExceptionHandler();
 
 app.Use(async (context, next) =>
 {
-    context.Response.Headers.Add("X-Developed-By", "Takerman Ltd");
+    // context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
     context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
-    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
     context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
+    context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+    context.Response.Headers.Add("X-Developed-By", "Takerman Ltd");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("X-Frame-Options", "DENY");
     context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-    context.Response.Headers.Add("Referrer-Policy", "no-referrer");
     context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
     context.Response.Headers.Add("X-Powered-By", "Pegasus");
-    await next.Invoke();
+    await next();
 });
 
 app.MapControllers();
