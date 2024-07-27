f9d831dfcb6d8ebee5508f22d076538fd2a7f4fb
Tanyo Ivanov <tivanov@takerman.net>
2024-07-27 21:11:31 +0300

    again

--- Takerman.Dating.Data/Initialization/ContextInitializer.cs
+++ Takerman.Dating.Data/Initialization/ContextInitializer.cs
@@ -1,7 +1,6 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
-using Google.Apis.Util.Store;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
